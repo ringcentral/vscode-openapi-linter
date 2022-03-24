34a33f52652ae7d6c0ffb3929751cc91f4079b1b
Tyler Long <tyler4long@gmail.com>
2022-03-23 18:07:04 -0700

    No configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,15 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 import {
 	createConnection,
 	TextDocuments,
-	Diagnostic,
 	DiagnosticSeverity,
 	ProposedFeatures,
 	InitializeParams,
-	DidChangeConfigurationNotification,
 	TextDocumentSyncKind,
 	InitializeResult
 } from 'vscode-languageserver/node';
-
 import {
 	TextDocument
 } from 'vscode-languageserver-textdocument';
-
 import {Spectral, Document, Ruleset} from '@stoplight/spectral-core';
 import {Yaml} from '@stoplight/spectral-parsers';
 import {oas, asyncapi} from '@stoplight/spectral-rulesets';
@@ -32,78 +24,13 @@ const connection = createConnection(ProposedFeatures.all);
 // Create a simple text document manager.
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
-let hasConfigurationCapability = false;
-
 connection.onInitialize((params: InitializeParams) => {
-	const capabilities = params.capabilities;
-
-	// Does the client support the `workspace/configuration` request?
-	// If not, we fall back using global settings.
-	hasConfigurationCapability = !!(
-		capabilities.workspace && !!capabilities.workspace.configuration
-	);
-
 	const result: InitializeResult = {
 		capabilities: {
 			textDocumentSync: TextDocumentSyncKind.Incremental,
 		}
 	};
 	return result;
-});
-
-connection.onInitialized(() => {
-	if (hasConfigurationCapability) {
-		// Register for all configuration changes.
-		connection.client.register(DidChangeConfigurationNotification.type, undefined);
-	}
-});
-
-// The extension settings
-interface ExtensionSettings {
-	maxNumberOfProblems: number;
-}
-
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings: ExtensionSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExtensionSettings = defaultSettings;
-
-// Cache the settings of all open documents
-const documentSettings: Map<string, Thenable<ExtensionSettings>> = new Map();
-
-connection.onDidChangeConfiguration(change => {
-	if (hasConfigurationCapability) {
-		// Reset all cached document settings
-		documentSettings.clear();
-	} else {
-		globalSettings = <ExtensionSettings>(
-			(change.settings.openApiLinter || defaultSettings)
-		);
-	}
-
-	// Revalidate all open text documents
-	documents.all().forEach(validateTextDocument);
-});
-
-function getDocumentSettings(resource: string): Thenable<ExtensionSettings> {
-	if (!hasConfigurationCapability) {
-		return Promise.resolve(globalSettings);
-	}
-	let result = documentSettings.get(resource);
-	if (!result) {
-		result = connection.workspace.getConfiguration({
-			scopeUri: resource,
-			section: 'openApiLinter'
-		});
-		documentSettings.set(resource, result);
-	}
-	return result;
-}
-
-// Only keep settings for open documents
-documents.onDidClose(e => {
-	documentSettings.delete(e.document.uri);
 });
 
 // The content of a text document has changed. This event is emitted
@@ -113,25 +40,15 @@ documents.onDidChangeContent(change => {
 });
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-	// In this simple example we get the settings for every validate run.
-	const settings = await getDocumentSettings(textDocument.uri);
-
-	// The validator creates diagnostics for all uppercase words length 2 and more
 	const text = textDocument.getText();
-
-	const diagnostics: Diagnostic[] = [];
 	const issues = await spectral.run(new Document(text, Yaml, 'spec.yaml'));
-	for(const issue of issues) {
-		const diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
+	const diagnostics = issues.map(issue => ({
+		severity: DiagnosticSeverity.Warning,
 			code: issue.code,
 			range: issue.range,
 			message: issue.message,
 			source: 'OpenAPI Linter'
-		};
-		diagnostics.push(diagnostic);
-	}
-
+	}));
 	// Send the computed diagnostics to VSCode.
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
