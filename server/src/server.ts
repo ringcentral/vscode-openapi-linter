9673680074e44ae93f6d67951b792d018e2457d8
Tyler Long <tyler4long@gmail.com>
2022-04-04 10:32:48 -0700

    Indent using 2 spaces

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
 import {
-	createConnection,
-	TextDocuments,
-	ProposedFeatures,
-	InitializeParams,
-	TextDocumentSyncKind,
-	InitializeResult,
-	Diagnostic
+  createConnection,
+  TextDocuments,
+  ProposedFeatures,
+  InitializeParams,
+  TextDocumentSyncKind,
+  InitializeResult,
+  Diagnostic
 } from 'vscode-languageserver/node';
 import {
-	TextDocument
+  TextDocument
 } from 'vscode-languageserver-textdocument';
 import {Spectral, Document, Ruleset} from '@stoplight/spectral-core';
 import {Yaml} from '@stoplight/spectral-parsers';
@@ -25,35 +25,35 @@ const connection = createConnection(ProposedFeatures.all);
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 connection.onInitialize((params: InitializeParams) => {
-	const result: InitializeResult = {
-		capabilities: {
-			textDocumentSync: TextDocumentSyncKind.Incremental,
-		}
-	};
-	return result;
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+    }
+  };
+  return result;
 });
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
-	validateTextDocument(change.document);
+  validateTextDocument(change.document);
 });
 
 async function validateTextDocument(textDocument: TextDocument): Promise<void> {
-	const text = textDocument.getText();
-	let diagnostics: Diagnostic[] = [];
-	if(text.startsWith('openapi:')) {
-		const issues = await spectral.run(new Document(text, Yaml, 'spec.yaml'));
-		diagnostics = issues.map(issue => ({
-			severity: issue.severity + 1,
-			code: issue.code,
-			range: issue.range,
-			message: issue.message,
-			source: 'OpenAPI Linter'
-		})) as Diagnostic[];
-	}
-	// Send the computed diagnostics to VSCode.
-	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+  const text = textDocument.getText();
+  let diagnostics: Diagnostic[] = [];
+  if(text.startsWith('openapi:')) {
+    const issues = await spectral.run(new Document(text, Yaml, 'spec.yaml'));
+    diagnostics = issues.map(issue => ({
+      severity: issue.severity + 1,
+      code: issue.code,
+      range: issue.range,
+      message: issue.message,
+      source: 'OpenAPI Linter'
+    })) as Diagnostic[];
+  }
+  // Send the computed diagnostics to VSCode.
+  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
 
 // Make the text document manager listen on the connection
